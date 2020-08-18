4b1113db0c4fd0840fd74f64a3315bd97ef3c29a
Jack Hsu <jack.hsu@gmail.com>
2020-08-17 22:48:09 -0400

    chore: update to Nx 10 and Angular 10

--- apps/okreads/api/jest.config.js
+++ apps/okreads/api/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   name: 'okreads-api',
   preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/apps/okreads/api'
+  coverageDirectory: '../../../coverage/apps/okreads/api',
+  globals: { 'ts-jest': { tsConfig: '<rootDir>/tsconfig.spec.json' } },
 };
