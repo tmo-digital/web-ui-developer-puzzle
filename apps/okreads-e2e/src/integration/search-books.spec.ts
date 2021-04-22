b390cef272a02aebb83bcb69ff33012c83abf07b
Jo Hanna Pearce <jo@nrwl.io>
2021-04-22 11:12:49 +0100

    chore: update e2e tests to use Cypress

---
+++ apps/okreads-e2e/src/integration/search-books.spec.ts
@@ -0,0 +1,17 @@
+describe('When: Use the search feature', () => {
+  beforeEach(() => {
+    cy.startAt('/');
+  });
+
+  it('Then: I should be able to search books by title', () => {
+    cy.get('input[type="search"]').type('javascript');
+
+    cy.get('form').submit();
+
+    cy.get('[data-testing="book-item"]').should('have.length.greaterThan', 1);
+  });
+
+  xit('Then: I should see search results as I am typing', () => {
+    // TODO: Implement this test!
+  });
+});
